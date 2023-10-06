0eaf2070e8b6dcceb04f6b801607a3ed5dcacd97
hast06001 <hast06001@E-PW024NW1>
2023-10-06 11:12:59 +0200

    Add project files.

---
+++ Uppgift 3.8/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Uppgift_3._8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ett ord.");
+            string ord1 = Console.ReadLine();
+            Console.WriteLine("Skriv in ett ord till.");
+            string ord2 = Console.ReadLine();
+            Console.WriteLine("Skriv in ett sista ord också.");
+            string ord3 = Console.ReadLine();
+
+            if(ord1.CompareTo(ord2) < 0 && ord1.CompareTo(ord3) < 0)
+                Console.WriteLine("Ord 1 kommer först i storleksordning.");
+            if(ord2.CompareTo(ord1) < 0 && ord2.CompareTo(ord3) < 0)
+                Console.WriteLine("Ord 2 kommer fört i storleksordning");
+            if (ord3.CompareTo(ord1) < 0 && ord3.CompareTo(ord2) < 0)
+                Console.WriteLine("Ord 3 kommer först i storleksordning");
+
+            Console.ReadKey();
+        }
+    }
+}
